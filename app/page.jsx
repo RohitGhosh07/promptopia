14313956edf2e89f254fd4850f04bc45a177abb9
ionicthor <ioniccompound6@gmail.com>
2023-12-03 22:21:19 +0530

    done updation

--- app/page.jsx
+++ app/page.jsx
@@ -5,11 +5,11 @@ const Home = () => (
     <h1 className='head_text text-center'>
       Discover & Share
       <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+      <span className='orange_gradient text-center'> Through Shared Thoughts</span>
     </h1>
     <p className='desc text-center'>
-      Promptopia is an open-source AI prompting tool for modern world to
-      discover, create and share creative prompts
+
+    Promptopia is a versatile platform where users are encouraged to freely express their 💭 thoughts, 🎨 create, and 📝 share a diverse array of creative prompts. ✨
     </p>
 
     <Feed />
